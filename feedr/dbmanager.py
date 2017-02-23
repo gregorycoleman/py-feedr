489da42e5bedc1653ec041e98afc2a12e681bcf9
shunyi <M157q.tw@gmail.com>
2017-02-24 06:28:06 +0800

    [#7] Init USER_SUBSCRIPTIONS_TABLE if not been created.

--- feedr/dbmanager.py
+++ feedr/dbmanager.py
@@ -1,6 +1,9 @@
 import sqlite3
 
 
+USER_SUBSCRIPTIONS_TABLE_NAME = 'user_subscriptions'
+
+
 class DatabaseManager(object):
 
     '''
@@ -28,9 +31,31 @@ class DatabaseManager(object):
         if c.fetchall() == []:  # table doesn't exist
             # ! WARNING : Vulnerable to SQLi with forged table name
             # Ugly workaround for binding a table name
-            c.execute(
-                'CREATE table {}(id integer primary key autoincrement, sha256_hash text, date text, title text, url text)'.
-                format(self.feed_dbtable))
+            if self.feed_dbtable == USER_SUBSCRIPTIONS_TABLE_NAME:
+                c.execute(
+                    (
+                        'CREATE table {}'
+                        '(id integer primary key autoincrement, '
+                        'user_id text, '
+                        'feed_table_name text)'
+                    ).format(
+                        self.feed_dbtable,
+                    )
+                )
+            else:
+                # feed table
+                c.execute(
+                    (
+                        'CREATE table {}'
+                        '(id integer primary key autoincrement, '
+                        'sha256_hash text, '
+                        'date text, '
+                        'title text, '
+                        'url text)'
+                    ).format(
+                        self.feed_dbtable,
+                    )
+                )
         conn.commit()
         conn.close()
 
